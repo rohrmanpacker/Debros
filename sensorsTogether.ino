ddc690ef21f735a057d46d08094db08dd29c693e
Nick Rohrman <rohrmanpacker@gmail.com>
2019-11-11 10:29:29 -0500

    Add files via upload

---
+++ sensorsTogether.ino
@@ -0,0 +1,68 @@
+#include "HX711.h"
+#include <Servo.h>
+
+//servo 
+Servo myservo1; 
+Servo myservo2; 
+
+
+// HX711 circuit wiring
+const int LOADCELL_DOUT_PIN = 2; 
+const int LOADCELL_SCK_PIN = 3;
+HX711 scale;
+
+int volt;
+String material;
+
+int pos = 0;
+int change = 2;
+
+void setup() {
+  Serial.begin(9600);
+  myservo1.attach(10);  // attaches the servo on pin 10 to the servo object
+  myservo1.write(pos);
+  myservo2.attach(11);  // attaches the servo on pin 11 to the servo object
+  myservo2.write(pos);
+
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  scale.set_scale();                      // this value is obtained by calibrating the scale with known weights; see the README for details
+  scale.tare();               // reset the scale to 0
+
+}
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  //servo
+  myservo1.write(pos);
+  myservo2.write(pos);
+  
+  pos+=change;
+  if(pos <= 0){change *=-1;}
+  if(pos >= 180){change *=-1;}
+  
+
+  //load cell
+  long reading = scale.get_units();
+  float weight = (2.19 * .001 * reading);  
+
+  //conductivity
+  volt = analogRead(1);
+  if(volt<512){
+    material = "metal";
+  }else {
+    material = "plastic";
+  }
+
+  //delay
+  delay(5);
+  
+  //print shit
+  Serial.print("position:\t");Serial.print(pos);Serial.print(" ");
+  Serial.print("reading:\t");Serial.print(reading);Serial.print(" weight:\t");Serial.print(weight);Serial.print(" Conductivity:\t");Serial.print(volt);Serial.print(" Material:\t");Serial.print(material);
+  Serial.println();
+  
+  
+}
